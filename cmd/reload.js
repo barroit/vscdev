3efdb82595fa63c5080879d1c11e2206f37aff3c
Jiamu Sun <barroit@linux.com>
2025-12-11 04:53:33 +0900

    init: setup repo from vscdevp
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ cmd/reload.js
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/*
+ * Copyright 2025 Jiamu Sun <barroit@linux.com>
+ */
+
+export function exec()
+{
+	console.log(111)
+}
---
+++ entry.js
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/*
+ * Copyright 2025 Jiamu Sun <barroit@linux.com>
+ */
+
+import { commands } from 'vscode'
+
+const { registerCommand: cmd } = commands
+
+const cmds = {
+	'reload': import('./cmd/reload.js'),
+}
+
+export async function activate(ctx)
+{
+	for (const id of Object.keys(cmds)) {
+		const module = await cmds[id]
+		const exec = cmd(`vscdev.${id}`, module.exec, { ctx })
+
+		ctx.subscriptions.push(exec)
+	}
+}
